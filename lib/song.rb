7b6e892ac2cbde53596ce1e0a6c2ae481dd28e88
Chris Gullickson <cgullickson5@gmail.com>
2017-11-29 20:51:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
     song.save
     song
   end
-  
+
   def self.find_by_name (name)
     @@all.detect do |song|
       song.name == name
@@ -36,7 +36,7 @@ class Song
   end
 
   def self.find_or_create_by_name (name)
-    self.find_by_name(name) == nil ? self.create_by_name (name) : self.find_by_name(name)
+    self.create_by_name (name) || self.find_by_name(name)
   end
   
 end
